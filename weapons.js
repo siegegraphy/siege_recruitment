080080922f6d488ad369c090518578f607740b87
siegegraphy <siegegraphy@gmail.com>
2025-09-07 18:57:40 +0200

    Create weapons.js

---
+++ weapons.js
@@ -0,0 +1,234 @@
+const primaryWeapons = {
+  "======== ASSAULT RIFLE ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: '416-C CARBINE', value: 'Weapons/416-C CARBINE.png' },
+      { name: '552 COMMANDO', value: 'Weapons/552 COMMANDO.png' },
+      { name: '556XI', value: 'Weapons/556XI.png' },
+      { name: 'AK-12', value: 'Weapons/AK-12.png' },
+      { name: 'AK-74M', value: 'Weapons/AK-74M.png' },
+      { name: 'AR33', value: 'Weapons/AR33.png' },
+      { name: 'ARX200', value: 'Weapons/ARX200.png' },
+      { name: 'AUG A2', value: 'Weapons/AUG A2.png' },
+      { name: 'C7E', value: 'Weapons/C7E.png' },
+      { name: 'C8-SFW', value: 'Weapons/C8-SFW.png' },
+      { name: 'COMMANDO 9', value: 'Weapons/COMMANDO 9.png' },
+      { name: 'F2', value: 'Weapons/F2.png' },
+      { name: 'F90', value: 'Weapons/F90.png' },
+      { name: 'G36C', value: 'Weapons/G36C.png' },
+      { name: 'L85A2', value: 'Weapons/L85A2.png' },
+      { name: 'M4', value: 'Weapons/M4.png' },
+      { name: 'M762', value: 'Weapons/M762.png' },
+      { name: 'MK17 CQB', value: 'Weapons/MK17 CQB.png' },
+      { name: 'PARA-308', value: 'Weapons/PARA-308.png' },
+      { name: 'PCX-33', value: 'Weapons/PCX-33.png' },
+      { name: 'POF9', value: 'Weapons/POF9.png' },
+      { name: 'R4-C', value: 'Weapons/R4-C.png' },
+      { name: 'SC3000K', value: 'Weapons/SC3000K.png' },
+      { name: 'SPEAR .308', value: 'Weapons/SPEAR .308.png' },
+      { name: 'TYPE-89', value: 'Weapons/TYPE-89.png' },
+      { name: 'V308', value: 'Weapons/V308.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [
+      { name: 'LVOA', value: 'Weapons/LVOA.png' },
+    ],
+  },
+  "======== LIGHT MACHINE GUN ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: '6P41', value: 'Weapons/6P41.png' },
+      { name: 'ALDA 5.56', value: 'Weapons/ALDA 5.56.png' },
+      { name: 'DP27', value: 'Weapons/DP27.png' },
+      { name: 'G8A1', value: 'Weapons/G8A1.png' },
+      { name: 'LMG-E', value: 'Weapons/LMG-E.png' },
+      { name: 'M249', value: 'Weapons/M249.png' },
+      { name: 'M249 SAW', value: 'Weapons/M249 SAW.png' },
+      { name: 'T-95 LSW', value: 'Weapons/T-95 LSW.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [ 
+    ],
+  },
+  "======== MARKSMAN RIFLE ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: '417', value: 'Weapons/417.png' },
+      { name: 'AR-15.50', value: 'Weapons/AR-15.50.png' },
+      { name: 'CAMRS', value: 'Weapons/CAMRS.png' },
+      { name: 'MK 14 EBR', value: 'Weapons/MK 14 EBR.png' },
+      { name: 'OTS-03', value: 'Weapons/OTS-03.png' },
+      { name: 'SR-25', value: 'Weapons/SR-25.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [
+      { name: 'DRAGUNOV SWD', value: 'Weapons/DRAGUNOV SWD.png' },
+    ],
+  },
+  "======== SNIPER RIFLE ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: 'CRSX 300', value: 'Weapons/CRSX 300.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [
+    ],
+  },
+  "======== SUBMACHINE GUN ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: '9MM C1', value: 'Weapons/9MM C1.png' },
+      { name: '9X19VSN', value: 'Weapons/9X19VSN.png' },
+      { name: 'AUG A3', value: 'Weapons/AUG A3.png' },
+      { name: 'FMG-9', value: 'Weapons/FMG-9.png' },
+      { name: 'K1A', value: 'Weapons/K1A.png' },
+      { name: 'M12', value: 'Weapons/M12.png' },
+      { name: 'MP5', value: 'Weapons/MP5.png' },
+      { name: 'MP5K', value: 'Weapons/MP5K.png' },
+      { name: 'MP5SD', value: 'Weapons/MP5SD.png' },
+      { name: 'MP7', value: 'Weapons/MP7.png' },
+      { name: 'MPX', value: 'Weapons/MPX.png' },
+      { name: 'MX4 STORM', value: 'Weapons/MX4 STORM.png' },
+      { name: 'P10 RONI', value: 'Weapons/P10 RONI.png' },
+      { name: 'P90', value: 'Weapons/P90.png' },
+      { name: 'PDW9', value: 'Weapons/PDW9.png' },
+      { name: 'SCORPION EVO 3 A1', value: 'Weapons/SCORPION EVO 3 A1.png' },
+      { name: 'T-5 SMG', value: 'Weapons/T-5 SMG.png' },
+      { name: 'UMP45', value: 'Weapons/UMP45.png' },
+      { name: 'UZK50GI', value: 'Weapons/UZK50GI.png' },
+      { name: 'VECTOR .45 ACP', value: 'Weapons/VECTOR .45 ACP.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [
+      { name: 'THOMPSON MK2', value: 'Weapons/THOMPSON MK2.png' },
+    ],
+  },
+  "======== SLUG SHOTGUN ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: 'ACS 12', value: 'Weapons/ACS 12.png' },
+      { name: 'BOSG. 12.2', value: 'Weapons/BOSG. 12.2.png' },
+      { name: 'TCSG12', value: 'Weapons/TCSG12.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [
+    ],
+  },
+  "======== SHOTGUN ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: 'FO-12', value: 'Weapons/FO-12.png' },
+      { name: 'ITA12L', value: 'Weapons/ITA12L.png' },
+      { name: 'M1014', value: 'Weapons/M1014.png' },
+      { name: 'M590A1', value: 'Weapons/M590A1.png' },
+      { name: 'M870', value: 'Weapons/M870.png' },
+      { name: 'SASG-12', value: 'Weapons/SASG-12.png' },
+      { name: 'SG-CQB', value: 'Weapons/SG-CQB.png' },
+      { name: 'SIX12', value: 'Weapons/SIX12.png' },
+      { name: 'SIX12 SD', value: 'Weapons/SIX12 SD.png' },
+      { name: 'SPAS-12', value: 'Weapons/SPAS-12.png' },
+      { name: 'SPAS-15', value: 'Weapons/SPAS-15.png' },
+      { name: 'SUPER 90', value: 'Weapons/SUPER 90.png' },
+      { name: 'SUPERNOVA', value: 'Weapons/SUPERNOVA.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [ 
+      { name: 'ESCORT BTS12', value: 'Weapons/ESCORT BTS12.png' },
+      { name: 'JACKHAMMER', value: 'Weapons/JACKHAMMER.png' },
+    ],
+  },
+  "======== SHIELD ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: 'BALLISTIC SHIELD', value: 'Weapons/BALLISTIC SHIELD.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [
+    ],
+  },
+};
+
+const secondaryWeapons = {
+  "======== SLUG SHOTGUN ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: 'GLAIVE-12', value: 'Weapons/GLAIVE-12.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [
+    ],
+  },
+  "======== SHOTGUN ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: 'ITA12S', value: 'Weapons/ITA12S.png' },
+      { name: 'SUPER SHORTY', value: 'Weapons/SUPER SHORTY.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [
+    ],
+  },
+  "======== MACHINE PISTOL ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: 'BEARING 9', value: 'Weapons/BEARING 9.png' },
+      { name: 'C75 AUTO', value: 'Weapons/C75 AUTO.png' },
+      { name: 'REAPER MK2', value: 'Weapons/REAPER MK2.png' },
+      { name: 'SMG-11', value: 'Weapons/SMG-11.png' },
+      { name: 'SMG-12', value: 'Weapons/SMG-12.png' },
+      { name: 'SPSMG9', value: 'Weapons/SPSMG9.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [
+    ],
+  },
+  "======== REVOLVER ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: '.44 VENDETTA', value: 'Weapons/44 VENDETTA.png' },
+      { name: 'BAILIFF 410', value: 'Weapons/BAILIFF 410.png' },
+      { name: 'KERATOS .357', value: 'Weapons/KERATOS .357.png' },
+      { name: 'LFP586', value: 'Weapons/LFP586.png' }   
+    ],
+    " -- CUSTOM WEAPON -- ": [
+    ],
+  },
+  "======== HANDGUN ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: '.44 MAG SEMI-AUTO', value: 'Weapons/44 MAG SEMI-AUTO.png' },
+      { name: '1911 TACOPS', value: 'Weapons/1911 TACOPS.png' },
+      { name: '5.7 USG', value: 'Weapons/5.7 USG.png' },
+      { name: 'D-50', value: 'Weapons/D-50.png' },
+      { name: 'GSH-18', value: 'Weapons/GSH-18.png' },
+      { name: 'LUISON', value: 'Weapons/LUISON.png' },
+      { name: 'M45 MEUSOC', value: 'Weapons/M45 MEUSOC.png' },
+      { name: 'MK1 9MM', value: 'Weapons/MK1 9MM.png' },
+      { name: 'P-10C', value: 'Weapons/P-10C.png' },
+      { name: 'P12', value: 'Weapons/P12.png' },
+      { name: 'P226 MK 25', value: 'Weapons/P226 MK 25.png' },
+      { name: 'P229', value: 'Weapons/P229.png' },
+      { name: 'P9', value: 'Weapons/P9.png' },
+      { name: 'PMM', value: 'Weapons/PMM.png' },
+      { name: 'PRB92', value: 'Weapons/PRB92.png' },
+      { name: 'Q-929', value: 'Weapons/Q-929.png' },
+      { name: 'RG15', value: 'Weapons/RG15.png' },
+      { name: 'SDP 9MM', value: 'Weapons/SDP 9MM.png' },
+      { name: 'USP40', value: 'Weapons/USP40.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [
+    ],
+  },
+  "======== HAND CANNON ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: 'GONNE-6', value: 'Weapons/GONNE-6.png' },
+    ],
+    " -- CUSTOM WEAPON -- ": [ 
+    ],
+  },
+};
+
+const gadgets = {
+  "======== ATTACK ========": {
+    " -- DEFAULT GADGET -- ": [
+      { name: 'BREACH CHARGE', value: 'Weapons/BREACH CHARGE.png' },
+      { name: 'CLAYMORE', value: 'Weapons/CLAYMORE.png' },
+      { name: 'FRAG GRENADE', value: 'Weapons/FRAG GRENADE.png' },
+      { name: 'HARD BREACH CHARGE', value: 'Weapons/HARD BREACH CHARGE.png' },
+      { name: 'SMOKE GRENADE', value: 'Weapons/SMOKE GRENADE.png' },
+      { name: 'STUN GRENADE', value: 'Weapons/STUN GRENADE.png' },
+      { name: 'IMPACT EMP GRENADE', value: 'Weapons/IMPACT EMP GRENADE.png' },
+    ],
+    " -- CUSTOM GADGET -- ": [
+    ],
+  },
+  "======== DEFENCE ========": {
+    " -- DEFAULT GADGET -- ": [
+      { name: 'BARBED WIRE', value: 'Weapons/BARBED WIRE.png' },
+      { name: 'BULLETPROOF CAMERA', value: 'Weapons/BULLETPROOF CAMERA.png' },
+      { name: 'DEPLOYABLE SHIELD', value: 'Weapons/DEPLOYABLE SHIELD.png' },
+      { name: 'OBSERVATION BLOCKER', value: 'Weapons/OBSERVATION BLOCKER.png' },
+      { name: 'IMPACT GRENADE', value: 'Weapons/IMPACT GRENADE.png' },
+      { name: 'NITRO CELL', value: 'Weapons/NITRO CELL.png' },
+      { name: 'PROXIMITY ALARM', value: 'Weapons/PROXIMITY ALARM.png' },
+    ],
+    " -- CUSTOM GADGET -- ": [
+    ],
+  },
+};
