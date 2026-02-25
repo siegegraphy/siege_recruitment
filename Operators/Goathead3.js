4e353c11dc8e67988359e58dc950eb8818771de0
siegegraphy <siegegraphy@gmail.com>
2026-02-25 15:16:50 +0100

    Create Goathead3.js

---
+++ Operators/Goathead3.js
@@ -0,0 +1,50 @@
+const operatorData = {
+  codename: "GOATHEAD2",
+  icon: "Icons/Goathead2.png",
+
+  side: {
+    text: "ATTACKER",
+    image: "Images/sideAttacker.png"
+  },
+
+  squad: {
+    text: "VIPERSTRIKE",
+    image: "Images/viperstrike.png"
+  },
+
+  specialty: "MAP CONTROL, INTEL",
+
+  health: "Images/healthspeedPoints1.png",
+  speed: "Images/healthspeedPoints3.png",
+
+  primaryWeapons: [
+    "Weapons/F90.png",
+    "Weapons/M870.png"
+  ],
+
+  secondaryWeapons: [
+    "Weapons/USP40.png"
+  ],
+
+  gadgets: [
+    "Weapons/BREACH CHARGE.png",
+    "Weapons/SMOKE GRENADE.png"
+  ],
+
+  uniqueAbility: "UniqueAbilities/BURR TRACKERS.png",
+
+  description: `Unique Ability: Burr Trackers x4
+
+Goathead throws a disc that bounces off walls.
+Upon landing it launches tracking devices.
+
+They ping only while moving.
+
+Additional Notes:
+- Intercepted by Jäger ADS
+- Destroyed by electrified surfaces
+- Frozen by Tubarão
+- Destroyed by Surya Gate`,
+
+  biography: ``
+};
