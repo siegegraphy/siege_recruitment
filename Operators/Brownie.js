18483a08688366802849f50a25df220a55f4295d
siegegraphy <siegegraphy@gmail.com>
2026-02-26 21:24:44 +0100

    Update Brownie.js

--- Operators/Brownie.js
+++ Operators/Brownie.js
@@ -1,1 +1,49 @@
+window.operatorData = {
+  codename: "HORUS",
+  icon: "Icons/Horus.png",
 
+  side: {
+    text: "DEFENDER",
+    image: "Images/sideDefender.png"
+  },
+
+  squad: {
+    text: "KERES",
+    image: "Images/keres.png"
+  },
+
+  specialty: "ANTI-ENTRY, CROWD CONTROL",
+
+  health: "Images/healthspeedPoints1.png",
+  speed: "Images/healthspeedPoints3.png",
+
+  flag: "Flags/Egypt.png",
+  country: "EGYPT",
+
+  realName: "[Undefined]",
+  dateOfBirth: "[Undefined]",
+  placeOfBirth: "Cairo, Egypt",
+
+  primaryWeapons: [
+    "Weapons/F2.png",
+    "Weapons/BTS12.png",
+    "Weapons/MP5K.png"
+  ],
+
+  secondaryWeapons: [
+    "Weapons/SMG-12.png",
+    "Weapons/44 MAG SEMI-AUTO.png",
+    "Weapons/C75 AUTO.png"
+  ],
+
+  gadgets: [
+    "Weapons/OBSERVATION BLOCKER.png",
+    "Weapons/IMPACT GRENADE.png"
+  ],
+
+  uniqueAbility: "UniqueAbilities/UniqueAbility.png",
+
+  description: ``,
+
+  biography: ``
+};
