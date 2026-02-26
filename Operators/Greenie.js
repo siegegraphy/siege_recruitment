55909b192e221631585a4a3841cfaaf3d2c44afd
siegegraphy <siegegraphy@gmail.com>
2026-02-26 21:25:27 +0100

    Update Greenie.js

--- Operators/Greenie.js
+++ Operators/Greenie.js
@@ -1,1 +1,49 @@
+window.operatorData = {
+  codename: "YAKOR",
+  icon: "Icons/Yakor.png",
 
+  side: {
+    text: "ATTACKER",
+    image: "Images/sideAttacker.png"
+  },
+
+  squad: {
+    text: "WOLFGUARD",
+    image: "Images/wolfguard.png"
+  },
+
+  specialty: "SUPPORT, ANTI-GADGET",
+
+  health: "Images/healthspeedPoints2.png",
+  speed: "Images/healthspeedPoints2.png",
+
+  flag: "Flags/Kazakhstan.png",
+  country: "KAZAKHSTAN",
+
+  realName: "[Undefined]",
+  dateOfBirth: "[Undefined]",
+  placeOfBirth: "Kazakhstan",
+
+  primaryWeapons: [
+    "Weapons/ARX200.png",
+    "Weapons/6P41.png"
+  ],
+
+  secondaryWeapons: [
+    "Weapons/SCORPION VZ.61.png",
+    "Weapons/GONNE-6.png",
+    "Weapons/GLAIVE-12.png"
+  ],
+
+  gadgets: [
+    "Weapons/FRAG GRENADE.png",
+    "Weapons/SMOKE GRENADE.png",
+    "Weapons/STUN GRENADE.png"
+  ],
+
+  uniqueAbility: "UniqueAbilities/UniqueAbility.png",
+
+  description: ``,
+
+  biography: ``
+};
