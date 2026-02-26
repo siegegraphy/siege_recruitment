70728e1185dac3bc963ef85c5a85afc2281bd9c9
siegegraphy <siegegraphy@gmail.com>
2026-02-26 14:57:57 +0100

    Update Horus.js

--- Operators/Horus.js
+++ Operators/Horus.js
@@ -1,1 +1,62 @@
+window.operatorData = {
+  codename: "HORUS",
+  icon: "Icons/Horus.png",
 
+  side: {
+    text: "DEFENDER",
+    image: "Images/sideDefender.png"
+  },
+
+  squad: {
+    text: "KERES",
+    image: "Images/keres.png"
+  },
+
+  specialty: "ANTI-ENTRY, CROWD CONTROL",
+
+  health: "Images/healthspeedPoints1.png",
+  speed: "Images/healthspeedPoints3.png",
+
+  flag: "Flags/Egypt.png",
+  country: "EGYPT",
+
+  realName: "[Undefined]",
+  dateOfBirth: "[Undefined]",
+  placeOfBirth: "Cairo, Egypt",
+
+  primaryWeapons: [
+    "Weapons/F2.png",
+    "Weapons/BTS12.png",
+    "Weapons/MP5K.png"
+  ],
+
+  secondaryWeapons: [
+    "Weapons/SMG-12.png",
+    "Weapons/44 MAG SEMI-AUTO.png",
+    "Weapons/C75 AUTO.png"
+  ],
+
+  gadgets: [
+    "Weapons/OBSERVATION BLOCKER.png",
+    "Weapons/IMPACT GRENADE.png"
+  ],
+
+  uniqueAbility: "UniqueAbilities/FALCON SNIPER STATION.png",
+
+  description: `Unique Ability: Remote Sniper Station "Falcon" 
+
+Horus has a remote sniper positioned outside the map, which effectively blocks attackers from entering the building through specific entry points. Horus uses a laser marker to designate a target point, and the remote sniper is aimed at that location. He can mark any spot outside or inside the building, as long as there is a nearby window, door, or other opening that allows the Falcon station to get a line of sight on the marked point.
+
+Once a target is marked, the remote sniper will lock onto it after 10 seconds, and a red line will appear, warning attackers. Any attacker who crosses the line will be shot by the sniper, entering a down-but-not-out state regardless of their health. If the attacker was already down-but-not-out, they will be eliminated instead. After a shot is fired, Falcon has a 5-second cooldown before it can fire again.
+
+Horus can change the sniper's target point at any moment, but after doing so, he must wait another 10 seconds for the Falcon system to reposition. The Falcon system is invulnerable, non-physical, and cannot be destroyed. Its only representation is the red line showing the shot's trajectory.
+
+The sniper's shot can be blocked by a shield facing the direction of the incoming shot. However, the attacker's shield will be knocked back, similar to a melee attack. Throwing an object into the sniper's laser will trigger the shot. The remote sniper remains active even if Horus is eliminated, but its position cannot be changed afterward.
+
+The purpose of this ability is to completely block attackers from entering through a specific location, forcing them to take an alternative route or sacrifice a throwable utility, punishing them if they attempt to push through. The ability to change targets allows Horus to adapt to the attackers' movements, adding dynamics to the gameplay. However, the limitation of only being able to hold one point at a time prevents Horus from stopping attackers during simultaneous attacks at multiple locations, rewarding coordinated teamwork while punishing solo actions taken by attackers.
+`,
+
+  biography: `Horus believed that punishment should be as harsh as the act that caused it. For him, justice wasn’t just about punishment, but also a means to restore balance, requiring not only courage but also precise determination. His views caught the attention of the Keres organization, which shared his strict principles and approach. Recognizing his potential to bring their vision to life, they gave him the name "Horus," after the deity symbolizing not only protection but also relentless strength.
+After the infiltration of the Keres base "Lair" by Rainbow operatives, Horus was reassigned to defend one of Keres' most important bases.
+`
+};
