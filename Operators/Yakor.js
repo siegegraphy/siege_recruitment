dd093878578b064f8dea576500183fad1ddbcb45
siegegraphy <siegegraphy@gmail.com>
2026-02-26 14:50:18 +0100

    Update Yakor.js

--- Operators/Yakor.js
+++ Operators/Yakor.js
@@ -1,1 +1,67 @@
+window.operatorData = {
+  codename: "YAKOR",
+  icon: "Icons/Yakor.png",
 
+  side: {
+    text: "ATTACKER",
+    image: "Images/sideAttacker.png"
+  },
+
+  squad: {
+    text: "WOLFGUARD",
+    image: "Images/wolfguard.png"
+  },
+
+  specialty: "SUPPORT, ANTI-GADGET",
+
+  health: "Images/healthspeedPoints2.png",
+  speed: "Images/healthspeedPoints2.png",
+
+  flag: "Flags/Kazakhstan.png",
+  country: "KAZAKHSTAN",
+
+  realName: "[Undefined]",
+  dateOfBirth: "[Undefined]",
+  placeOfBirth: "Kazakhstan",
+
+  primaryWeapons: [
+    "Weapons/ARX200.png",
+    "Weapons/6P41.png"
+  ],
+
+  secondaryWeapons: [
+    "Weapons/SCORPION VZ.61.png",
+    "Weapons/GONNE-6.png",
+    "Weapons/GLAIVE-12.png"
+  ],
+
+  gadgets: [
+    "Weapons/FRAG GRENADE.png",
+    "Weapons/SMOKE GRENADE.png",
+    "Weapons/STUN GRENADE.png"
+  ],
+
+  uniqueAbility: "UniqueAbilities/UNIVERSAL PROJECTILE LAUNCHER.png",
+
+  description: `Universal Projectile Launcher
+
+Yakor deploys a device that can be attached to any surface. When a projectile is thrown or fired near the device, it will be intercepted and stored inside. Yakor can then remotely activate any deployed device to launch the stored projectile.
+
+When setting up the U.P.L., Yakor can choose whether the projectile will be fired back to the same side of the surface or through to the opposite side - provided the surface is penetrable.
+
+The device can capture both attacker and defender projectiles:
+- Defender projectiles are intercepted if they pass within 1 meter of the device.
+- Attacker projectiles must hit the device directly to be captured.
+The device can only store one projectile at a time. It can be picked up and redeployed elsewhere, but if it is holding a projectile when picked up, the stored object will be destroyed. If a captured projectile has a timer, it is paused during storage and resets once fired again. Projectiles that require activation will trigger shortly after being launched.
+
+This ability is designed to let Yakor prepare a variety of projectiles and special abilities in advance, so that after repositioning, they can be remotely triggered for a coordinated attack. Alternatively, the device can be placed in key locations to intercept and neutralize enemy projectiles. The extended interception range for enemy projectiles increases the chances of capturing them, while the requirement for allied projectiles to hit the device directly reduces the risk of accidental interception.
+
+Special Interactions
+- The U.P.L. can be destroyed by any method of gadget destruction.
+- Countered by: EMP, Signal Disruptor, Zoto Canister.
+- Can fire through: Castle’s Armor Panel, Osa’s Talon-8 Shield.
+- Detected by Solis’ SPEC-IO Sensor.
+`,
+
+  biography: ``
+};
