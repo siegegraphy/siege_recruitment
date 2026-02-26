ee1906e8697dda301e0a99e2babdca9b208d817f
siegegraphy <siegegraphy@gmail.com>
2026-02-26 15:05:49 +0100

    Update Thanatos.js

--- Operators/Thanatos.js
+++ Operators/Thanatos.js
@@ -1,1 +1,61 @@
+window.operatorData = {
+  codename: "THANATOS",
+  icon: "Icons/Thanatos.png",
 
+  side: {
+    text: "DEFENDER",
+    image: "Images/sideDefender.png"
+  },
+
+  squad: {
+    text: "KERES",
+    image: "Images/keres.png"
+  },
+
+  specialty: "INTEL, TRAPPER",
+
+  health: "Images/healthspeedPoints1.png",
+  speed: "Images/healthspeedPoints3.png",
+
+  flag: "Flags/Earth.png",
+  country: "[UNDEFINED]",
+
+  realName: "[Undefined]",
+  dateOfBirth: "[Undefined]",
+  placeOfBirth: "[Undefined]",
+
+  primaryWeapons: [
+    "Weapons/UZK50GI.png",
+    "Weapons/VECTOR .45 ACP.png"
+  ],
+
+  secondaryWeapons: [
+    "Weapons/M45 MEUSOC.png",
+    "Weapons/SUPER SHORTY.png",
+    "Weapons/TESS-14.png"
+  ],
+
+  gadgets: [
+    "Weapons/BARBED WIRE.png",
+    "Weapons/DEPLOYABLE SHIELD.png",
+    "Weapons/NITRO CELL.png"
+  ],
+
+  uniqueAbility: "UniqueAbilities/FLAMBEAU SYSTEM.png",
+
+  description: `Flambeau System (x5)
+  
+Thanatos deploys a small dart launcher with an integrated tracking beacon, which can be placed on any surface.
+
+When an enemy enters its 2-meter detection range, the launcher automatically fires a tracking dart at that enemy. Once hit, the enemy’s position is fully revealed while they remain within a 10-meter radius of the launcher that fired the dart. The enemy is no longer revealed if they move outside this range or if the original launcher is destroyed. Leaving the range and re-entering it will reveal the enemy again.
+
+This tracking effect persists on the enemy for the remainder of the round.
+
+An enemy can be marked by multiple tracking darts and will be revealed whenever they enter the 10-meter radius of any corresponding launcher. Each launcher can fire only one dart.
+
+The gadget can be destroyed by any source of damage.
+Freezing or jamming the launcher temporarily disables the reveal effect for the duration of the interference.
+`,
+
+  biography: ``
+};
