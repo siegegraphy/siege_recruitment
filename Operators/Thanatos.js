c649ecaba49923b82a856ac43df36b21c11beb58
siegegraphy <siegegraphy@gmail.com>
2026-07-30 14:37:05 +0200

    Update Thanatos.js

--- Operators/Thanatos.js
+++ Operators/Thanatos.js
@@ -45,16 +45,24 @@ window.operatorData = {
 
   description: `Flambeau System (x5)
   
-Thanatos deploys a small dart launcher with an integrated tracking beacon, which can be placed on any surface.
+Thanatos deploys a small dart launcher with an integrated tracking beacon that can be attached to any surface.
 
-When an enemy enters its 2-meter detection range, the launcher automatically fires a tracking dart at that enemy. Once hit, the enemy’s position is fully revealed while they remain within a 10-meter radius of the launcher that fired the dart. The enemy is no longer revealed if they move outside this range or if the original launcher is destroyed. Leaving the range and re-entering it will reveal the enemy again.
+When an enemy enters its 2-meter detection radius, the launcher automatically fires a tracking dart at them and switches to beacon mode. Once hit, the enemy's position and operator identity are continuously revealed while they remain within a 10-meter radius of the launcher that fired the dart. If they leave this radius, they are no longer revealed. Re-entering the radius causes them to be revealed again.
 
-This tracking effect persists on the enemy for the remainder of the round.
+The tracking effect persists for the remainder of the round or until the corresponding beacon is destroyed.
 
-An enemy can be marked by multiple tracking darts and will be revealed whenever they enter the 10-meter radius of any corresponding launcher. Each launcher can fire only one dart.
+An enemy can be marked by multiple tracking darts and will be revealed whenever they enter the 10-meter radius of any corresponding beacon. Each launcher can fire only one dart.
 
-The gadget can be destroyed by any source of damage.
-Freezing or jamming the launcher temporarily disables the reveal effect for the duration of the interference.
+The dart launcher can be destroyed by any source of gadget destruction as long as it has not fired its dart. Once activated, however, it switches to beacon mode. In this state, it becomes bulletproof and can only be destroyed by explosions.
+
+Interactions:
+- Detected by IQ's Electronics Detector.
+- If frozen or jammed before firing, the launcher cannot fire its dart for the duration of the interference.
+- If frozen or jammed while in beacon mode, the reveal effect is temporarily disabled for the duration of the interference.
+- Players standing within a frozen or jammed area are not revealed.
+- While in launcher mode, it can be hacked by Brava.
+- While in beacon mode, it is destroyed if hacked by Brava.
+- Cannot fire its dart at enemies protected by a shield.
 `,
 
   biography: ``
