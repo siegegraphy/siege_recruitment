bd0b9d86a22fdd5d80c5e002101c7c0274341637
siegegraphy <siegegraphy@gmail.com>
2026-09-21 10:38:54 +0200

    Create Dial.js

---
+++ Operators/Dial.js
@@ -0,0 +1,48 @@
+window.operatorData = {
+  codename: "DIAL",
+  icon: "Icons/Dial.png",
+
+  side: {
+    text: "ATTACKER",
+    image: "Images/sideAttacker.png"
+  },
+
+  squad: {
+    text: "ARASSAS",
+    image: "Images/arassas.png"
+  },
+
+  specialty: "BREACH, ANTI-GADGET",
+
+  health: "Images/healthspeedPoints2.png",
+  speed: "Images/healthspeedPoints2.png",
+
+  flag: "Flags/Earth.png",
+  country: "[UNDEFINED]",
+
+  realName: "[Undefined]",
+  dateOfBirth: "[Undefined]",
+  placeOfBirth: "[Undefined]",
+
+  primaryWeapons: [
+    "Weapons/THOMPSON TYPE.PDW.png",
+    "Weapons/THOMPSON MK2.png"
+  ],
+
+  secondaryWeapons: [
+    "Weapons/SUPER SHORTY.png",
+    "Weapons/MP-9.png"
+  ],
+
+  gadgets: [
+    "Weapons/CLAYMORE.png",
+    "Weapons/FRAG GRENADE.png",
+    "Weapons/SMOKE GRENADE.png"
+  ],
+
+  uniqueAbility: "UniqueAbilities/UniqueAbility.png",
+
+  description: ``,
+
+  biography: ``
+};
